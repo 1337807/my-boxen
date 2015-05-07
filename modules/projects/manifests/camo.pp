6ffac0bfb7ea744b12c459c033e535f21f4a3930
Corey Donohoe <atmos@atmos.org>
2015-05-06 17:19:29 -0700

    yay

--- modules/projects/manifests/camo.pp
+++ modules/projects/manifests/camo.pp
@@ -2,7 +2,7 @@ class projects::camo {
   boxen::project { 'camo':
     dir    => '~/p/camo',
     dotenv => true,
-    nodejs => '0.10.29',
+    nodejs => 'v0.10.29',
     source => 'atmos/camo'
   }
 }
