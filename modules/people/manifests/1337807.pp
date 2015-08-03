3e3ca136e5ef74504ecbe1e4ddb06efe48a377ad
Jonan Scheffler <jonanscheffler@gmail.com>
2015-08-03 13:02:39 -0700

    Remove git aliases, unnecessary directories and links.

--- modules/people/manifests/1337807.pp
+++ modules/people/manifests/1337807.pp
@@ -8,21 +8,9 @@ class people::1337807 (
   create_resources(boxen::project, $private_projects)
 
   $home     = '/Users/jonan'
-  $dotfiles = "${home}/p/dotfiles"
+  $dotfiles = "${home}/dotfiles"
 
   git::config::global {
-    'alias.st':
-      value => 'status';
-    'alias.co':
-      value => 'checkout';
-    'alias.cp':
-      value => 'cherry-pick';
-    'alias.dc':
-      value => 'diff --cached';
-    'alias.lp':
-      value => 'log -p';
-    'alias.ro':
-      value => '!git fetch origin --prune && git reset --hard origin/master';
     'apply.whitespace':
       value => 'fix';
     'color.ui':
@@ -43,61 +31,14 @@ class people::1337807 (
       value => 'Jonan Scheffler';
   }
 
-  file {
-    ["${home}/.vim/swapfiles"]:
-      ensure => directory;
-  }
-
-  file {
-    ["${home}/p"]:
-      ensure => directory;
-  }
-
   repository { $dotfiles:
     path    => $dotfiles,
-    source  => '1337807/dotfiles',
-    require => File["${home}/p"]
+    source  => '1337807/dotfiles'
   }
 
   repository { $dotvim:
-    path    => "${home}/p/dotvim",
-    source  => "1337807/dotfiles",
-    require => File["${home}/p"]
-  }
-
-  file { "${home}/.vimrc":
-    ensure  => link,
-    force   => true,
-    target  => "${dotfiles}/vim/vimrc",
-    require => Repository[$dotfiles]
-  }
-
-  file { "${home}/.gvimrc":
-    ensure  => link,
-    force   => true,
-    target  => "${dotfiles}/vim/gvimrc",
-    require => Repository[$dotfiles]
-  }
-
-  file { "${home}/.atom":
-    ensure  => link,
-    force   => true,
-    target  => "${dotfiles}/atom",
-    require => Repository[$dotfiles]
-  }
-
-  file { "${home}/.vim":
-    ensure  => link,
-    force   => true,
-    target  => "${dotfiles}/vim/dotvim",
-    require => Repository[$dotfiles]
-  }
-
-  file { "${home}/bin":
-    ensure  => link,
-    force   => true,
-    target  => "${dotfiles}/bin",
-    require => Repository[$dotfiles]
+    path    => "${home}/dotvim",
+    source  => "1337807/dotfiles"
   }
 
   file { '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin':
