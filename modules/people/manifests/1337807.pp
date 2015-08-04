505e819bb2af5c00ea740d57dc87c99f004fc56e
Jonan Scheffler <jonanscheffler@gmail.com>
2015-08-03 22:51:14 -0700

    OSX configuration.

--- modules/people/manifests/1337807.pp
+++ modules/people/manifests/1337807.pp
@@ -12,6 +12,48 @@ class people::1337807 (
   $dotfiles = "${home}/src/dotfiles"
   $ohmyfish = "${home}/src/oh-my-fish"
 
+  osx::recovery_message { 'If found call 503-985-6626, email jonanscheffler@gmail.com or tweet @1337807': }
+
+  include osx::global::disable_key_press_and_hold
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::disable_autocorrect
+  include osx::global::natural_mouse_scrolling
+
+  include osx::finder::empty_trash_securely
+  include osx::finder::show_hidden_files
+  include osx::finder::enable_quicklook_text_selection
+  include osx::finder::show_warning_before_emptying_trash
+
+  include osx::disable_app_quarantine
+  include osx::no_network_dsstores
+  include osx::software_update
+
+  class { 'osx::mouse::button_mode':
+    mode => 2
+  }
+
+  class { 'osx::global::key_repeat_delay':
+    delay => 15
+  }
+
+  class { 'osx::global::key_repeat_rate':
+    rate => 2
+  }
+
+  class { 'osx::sound::interface_sound_effects':
+    enable => false
+  }
+
+  class { 'osx::dock::hot_corners':
+    top_right => "Start Screen Saver",
+  }
+
+  class { 'osx::mouse::swipe_between_pages':
+    enabled => true
+  }
+
   git::config::global {
     'apply.whitespace':
       value => 'fix';
@@ -44,15 +86,15 @@ class people::1337807 (
   }
 
   package { 'VirtualBox':
-      ensure => installed,
-      source => 'http://download.virtualbox.org/virtualbox/5.0.0/VirtualBox-5.0.0-101573-OSX.dmg',
-      provider => pkgdmg
+    ensure => installed,
+    source => 'http://download.virtualbox.org/virtualbox/5.0.0/VirtualBox-5.0.0-101573-OSX.dmg',
+    provider => pkgdmg
   }
 
   package { 'TeamViewer':
-      ensure => installed,
-      source => 'http://download.teamviewer.com/download/TeamViewer.dmg',
-      provider => pkgdmg
+    ensure => installed,
+    source => 'http://download.teamviewer.com/download/TeamViewer.dmg',
+    provider => pkgdmg
   }
 
   file { '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin':
@@ -64,6 +106,12 @@ class people::1337807 (
     ensure => link,
     force  => true,
     target => $ohmyfish
+  }
+
+  file { "${home}/.config/fish/config.fish":
+    ensure => link,
+    force  => true,
+    target => "${dotfiles}/fish/config.fish"
   }
 
   file { "${home}/.vim":
