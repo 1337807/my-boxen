0203e0af335399f5c06d883e803dca24c232286d
Jonan Scheffler <jonan@heroku.com>
2015-08-11 09:37:03 -0700

    Alias for git hist.

--- modules/people/manifests/1337807.pp
+++ modules/people/manifests/1337807.pp
@@ -60,6 +60,8 @@ class people::1337807 (
   }
 
   git::config::global {
+    'alias.hist':
+      value => "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
     'apply.whitespace':
       value => 'fix';
     'color.ui':
